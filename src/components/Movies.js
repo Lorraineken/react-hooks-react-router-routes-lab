8200ebbfa4b573f5f8c1f6ea67d1614baf5cb808
Lorraineken <lorrainekupa@gmail.com>
2023-01-20 14:07:32 +0300

    Updated Movies Page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const moviesDetail = movies.map((item)=>{
+
+    return(
+      <div>
+        {item.title}
+        {item.time}
+        <ul>
+         {item.genres.map((data) => <li>{data}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+  return (<div>
+    <h1>Movies Page</h1>
+     {moviesDetail}
+    
+  </div>);
 }
 
 export default Movies;
