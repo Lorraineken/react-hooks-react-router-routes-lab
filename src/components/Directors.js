c94c32f1e75881df5bb668c2202229ef287984dc
Lorraineken <lorrainekupa@gmail.com>
2023-01-20 14:21:18 +0300

    Updated Directors component

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+ const directorsDetails = directors.map((data) =>{
+  return(
+  <div>
+    {data.name}
+    <ul>
+     {data.movies.map((item) => <li>{item}</li>)} 
+    </ul>
+  </div>
+  )
+ })
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorsDetails}
+    </div>);
 }
 
 export default Directors;
